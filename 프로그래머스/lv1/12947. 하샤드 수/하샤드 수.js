dd92f220b90e9641b09fb5e6dae42193c03f8e57
황진성 <81561252+hwangjinseong@users.noreply.github.com>
2022-09-27 09:43:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,8 @@
+function solution(x) {
+    var sum = 0;
+    var arr = String(x).split("");
+    for(let i=0; i<arr.length; i++) {
+        sum += Number(arr[i])
+    }
+    return (x % sum == 0) ? true:false;
+}
