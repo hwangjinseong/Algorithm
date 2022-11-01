c8b9ebc128fe031a3f557f6900f60218db827449
황진성 <81561252+hwangjinseong@users.noreply.github.com>
2022-11-01 19:46:43 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = []
+    for(let i = 0 ; i < numbers.length; i ++) {
+        for(let j = 0 ; j < numbers.length; j++) {
+            if(i !== j) {
+                answer.push(numbers[i] * numbers[j])    
+            }
+        }
+    }
+    
+    //최댓값 출력
+    return Math.max(...answer)
+}
